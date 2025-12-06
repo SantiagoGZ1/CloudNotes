aa7bc1736a260e68df6d35fec94694cae6933f56
santiago <santi10gz10@gmail.com>
2025-12-06 14:50:10 -0500

    corrección anotaciones

--- src/main/java/com/cloud/cloudnotes/note_attachment/NoteAttachment.java
+++ src/main/java/com/cloud/cloudnotes/note_attachment/NoteAttachment.java
@@ -2,12 +2,14 @@ package com.cloud.cloudnotes.note_attachment;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSecondaryPartitionKey;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @DynamoDbBean
 public class NoteAttachment {
     private String noteAttachmentId;
