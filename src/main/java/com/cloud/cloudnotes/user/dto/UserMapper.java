f70061059c177b5a5a3b889e561db8351563a2cd
santiago <santi10gz10@gmail.com>
2025-12-17 15:20:03 -0500

    Dto

---
+++ src/main/java/com/cloud/cloudnotes/user/dto/UserMapper.java
@@ -0,0 +1,22 @@
+package com.cloud.cloudnotes.user.dto;
+
+import com.cloud.cloudnotes.user.UserEntity;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class UserMapper {
+
+    public static UserEntity toEntity(RegisterDto dto) {
+        if (dto == null) return null;
+        UserEntity entity = new UserEntity();
+        entity.setUsername(dto.getUsername());
+        entity.setPassword(dto.getPassword());
+        return entity;
+    }
+
+    public static RegisterDto toDto(UserEntity entity) {
+        if (entity == null) return null;
+        return new RegisterDto(entity.getUsername(), entity.getPassword());
+    }
+}
